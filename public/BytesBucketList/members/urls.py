3ccda6451cf3cc1be1e338c1ec1df3fc9b38c15e
Tommy <tjfricano@gmail.com>
2023-02-06 00:20:03 -0500

    cleaning

--- public/BytesBucketList/members/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.user_list, name='user-list'),
-    path('create/', views.create_user, name='create-user'),
-    path('retrieve/', views.retrieve, name = 'retrieve'),
-]
